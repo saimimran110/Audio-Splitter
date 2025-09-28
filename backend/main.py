029e46b5bf0a2c7d241251524205440fe510af39
saim <cym786@gmail.com>
2025-09-28 17:29:54 +0500

    deployed

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,7 @@ from fastapi.staticfiles import StaticFiles
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:8080", "http://localhost:3000"],
+    allow_origins=["*"],  # Allow all origins for now
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
